fc5545564d4e60d2c3203a06078dacbe16a2a380
aFFekopp <51753523+aFFekopp@users.noreply.github.com>
2020-01-16 20:16:57 +0100

    initial commit

---
+++ www/hui-view-settings.js
@@ -0,0 +1,19 @@
+customElements.whenDefined("hui-view").then( () => {
+    const HuiView = customElements.get("hui-view").prototype;
+    const oldRenderStyles = HuiView.renderStyles;
+    HuiView.renderStyles = function() {
+      let original = oldRenderStyles();
+      original.strings = [original.strings[0] + `
+      <style>
+        .column {
+          overflow: unset !important;
+          padding-left: 9px;
+          padding-right: 9px;
+          padding-bottom: 9px;
+        }
+      </style>
+      `];
+      return original;
+    }
+    fireEvent('ll-rebuild', {});
+    });
