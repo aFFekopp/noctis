e471f8bce840d9d99c1ce8e02c0959c9913144b0
aFFekopp <51753523+aFFekopp@users.noreply.github.com>
2020-04-25 10:11:32 +0200

    Update hui-view-settings.js

--- www/hui-view-settings.js
+++ www/hui-view-settings.js
@@ -1,19 +1,18 @@
 customElements.whenDefined("hui-view").then( () => {
-    const HuiView = customElements.get("hui-view").prototype;
-    const oldRenderStyles = HuiView.renderStyles;
-    HuiView.renderStyles = function() {
-      let original = oldRenderStyles();
-      original.strings = [original.strings[0] + `
-      <style>
-        .column {
-          overflow: unset !important;
-          padding-left: 9px;
-          padding-right: 9px;
-          padding-bottom: 9px;
-        }
-      </style>
-      `];
-      return original;
-    }
-    fireEvent('ll-rebuild', {});
-    });
+  const HuiView = customElements.get("hui-view").prototype;
+  const oldRenderStyles = HuiView.renderStyles;
+  HuiView.renderStyles = function() {
+    let original = oldRenderStyles();
+    original.strings = [original.strings[0] + `
+    <style>
+      .column {
+        overflow: unset !important;
+        padding-left: 9px;
+        padding-right: 9px;
+        padding-bottom: 9px;
+      }
+    </style>
+    `];
+    return original;
+  }
+});
